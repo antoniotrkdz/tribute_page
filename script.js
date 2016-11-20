6b7037141e31d527ca5f2730d111cb9f8088af25
antoniotrkdz <antoniotrkdz@localhost.localdomain>
2016-11-20 03:18:23 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 <!--
 window.onload = function()
 {
-  var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-  d.getElementById('report').innerHTML = x + " x " + y;
+  //var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+  var o=document.getElementById('content-text').offsetHeight;
+  document.getElementById('report').innerHTML = o;
+
  //document.write(x + " x " + y);
 }
 //-->
