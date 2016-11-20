02f737cd96750d467c5a7f167aeac744ebf3069e
antoniotrkdz <antoniotrkdz@localhost.localdomain>
2016-11-20 02:37:03 +0000

    modified:   index.html 	new file:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,8 @@
+<!--
+window.onload = function()
+{
+  var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+  d.getElementById('report').innerHTML = x + " x " + y;
+ //document.write(x + " x " + y);
+}
+//-->
